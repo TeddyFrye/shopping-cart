affa0d3bd3c299f1e8d82c769ce9c8f12498248e
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2023-11-09 11:25:15 -0600

    Adds loading  scroll  for shopping page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,27 @@ import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("https://fakestoreapi.com/products?limit=6")
       .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((err) => console.error(err)); // Handle errors
+      .then((data) => {
+        setProducts(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLoading(false);
+      });
   }, []);
+
   return (
     <div className="home">
-      <h1>Welcome to the Shopping Cart App</h1>
-      <p>Find the best products for all your needs.</p>
+      <h1>Welcome to the Shopping</h1>
+      <p>Find best products for all of your needings.</p>
+      {isLoading ? <p>Loading...</p> : null}
       <div className="product-list">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,18 +1,42 @@
 import { useState, useEffect } from "react";
 import ProductCard from "../components/ProductCard";
+import "../styles/Shop.css";
+import "../styles/ProductCard.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [displayedProducts, setDisplayedProducts] = useState([]);
+  const itemsPerPage = 6;
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        setProducts(data);
+        setDisplayedProducts(data.slice(0, itemsPerPage));
+      });
   }, []);
 
+  const handleScroll = (e) => {
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight;
+    if (bottom) {
+      setPage((prevPage) => prevPage + 1);
+      const newItems = products.slice(0, page * itemsPerPage);
+      setDisplayedProducts(newItems);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [page, products]);
+
   return (
-    <div>
-      {products.map((product) => (
+    <div className="shop-container">
+      {displayedProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>
