63a2678a8ecfcfede170b3709922b47c4a56b59b
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2023-11-09 10:50:40 -0600

    Adds product  cards to  home page and connects to FakeStoreAPI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
+import ProductCard from "../components/ProductCard";
+
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=6")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error(err)); // Handle errors
+  }, []);
   return (
     <div className="home">
       <h1>Welcome to the Shopping Cart App</h1>
       <p>Find the best products for all your needs.</p>
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
-
 export default Home;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,29 +1,20 @@
 import { useState, useEffect } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    // Placeholder for fetching products logic
     fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
 
   return (
-    <div className="shop">
-      <h1>Shop Our Products</h1>
-      <div className="product-list">
-        {products.map((product) => (
-          <div key={product.id} className="product">
-            <h2>{product.title}</h2>
-            <img src={product.image} alt={product.title} />
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-            {/* Add to Cart functionality will go here */}
-          </div>
-        ))}
-      </div>
+    <div>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 };
