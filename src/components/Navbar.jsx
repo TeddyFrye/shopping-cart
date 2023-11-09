a53eaa3e74823de971cf655e6fb9aa171ebb3822
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2023-11-09 11:24:49 -0600

    Adds link to Home  page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useCart } from "./CartSummary";
+import { Link } from "react-router-dom";
 
 const CartSummary = () => {
   const { cartItems } = useCart();
@@ -10,8 +11,10 @@ const CartSummary = () => {
 
   return (
     <div>
-      <p>Cart Items: {totalItems}</p>
-      {/* Other cart summary info */}
+      <p>Cart Items: {totalItems > 0 ? totalItems : "0"}</p>
+      <Link to="/">Home </Link>
+      <p> </p>
+      <Link to="/shop">Shop</Link>
     </div>
   );
 };
