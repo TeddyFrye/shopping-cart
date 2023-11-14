6d4646dc3b2522b67090780a3d54f4499f81ebf5
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2023-11-14 15:54:41 -0600

    Adds further tests

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -2,7 +2,9 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 import App from "../src/App";
+import ProductCard from "../src/components/ProductCard";
 
+// Mocks
 vi.mock("react-router-dom", async () => {
   const actualRouterDom = await vi.importActual("react-router-dom");
   return {
@@ -11,6 +13,23 @@ vi.mock("react-router-dom", async () => {
   };
 });
 
+vi.mock("node-fetch", async () => {
+  return {
+    default: async () => ({
+      json: async () => [{ id: 1, title: "Test Product", price: 9.99 }],
+    }),
+  };
+});
+
+vi.mock('../src/components/CartSummary.jsx', async () => {
+  useCart:  () => {
+    addToCart: vi.fn(),
+    removeFromCart: vi.fn(),
+    cartItems: [],
+  }
+});
+
+// Test cases
 describe("App Component", () => {
   it("renders without crashing", () => {
     render(<App />, { wrapper: MemoryRouter });
@@ -35,5 +54,29 @@ describe("Route Navigation", () => {
     render(<App />, { wrapper: MemoryRouter });
     const shopLink = screen.getByRole("link", { name: /shop/i });
     fireEvent.click(shopLink);
+  });
+});
+
+//  Test product card
+describe("ProductCard Component", () => {
+  const mockProduct = {
+    id: 1,
+    title: "Test Product",
+    price: 9.99,
+    image: "test-image-url",
+  };
+
+  it("renders product information", () => {
+    render(<ProductCard product={mockProduct} />);
+    expect(screen.getByText("Test Product")).toBeInTheDocument();
+    expect(screen.getByText("$9.99")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toHaveAttribute("src", "test-image-url");
+  });
+
+  it("enlarges the image on click", () => {
+    render(<ProductCard product={mockProduct} />);
+    const image = screen.getByRole("img");
+    fireEvent.click(image);
+    expect(image).toHaveClass("enlarged");
   });
 });
