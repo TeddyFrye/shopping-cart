fa374189cdfb6d39e4f695feacf078c2dd9dd04f
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2023-11-17 13:17:14 -0600

    Adds Tarcet styling  and Terms page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,22 @@ import "../styles/Navbar.css";
 const NavBar = () => {
   return (
     <div className="navbar">
+      <div className="logo">
+        <img
+          src="../../public/target logo.png"
+          alt="target logo"
+          className="logo"
+        ></img>
+        <h1 className="navbar-title">TARCET</h1>
+      </div>
       <Link to="/" className="navbar-link">
         Home
       </Link>
       <Link to="/shop" className="navbar-link">
         Shop
+      </Link>
+      <Link to="/terms" className="navbar-link">
+        Terms
       </Link>
     </div>
   );
